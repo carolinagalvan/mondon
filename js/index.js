e984ada5529370745ea0bf24e16d5420b593bd4d
isabelaescalante <isaescalante123@gmail.com>
2019-04-28 15:59:49 -0500

    Style added to files

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ $('#categories').on('click', '.collapsible', function(event){
     else{
       let scroll = content.prop('scrollHeight');
       content.css("max-height", `${scroll}px`);
-    } 
+    }
 });
 
 
@@ -23,3 +23,34 @@ $('#categories').on('click', '.addProduct', function(event){
     let scroll = content.prop('scrollHeight');
     content.css("max-height", `${scroll}px`);
 });
+
+// Script to open and close sidebar
+function open() {
+  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function close() {
+  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
+
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
